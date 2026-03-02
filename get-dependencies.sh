2ef401de1ab9ffee654e6ee5444044999ba200e3
Link4Electronics <link4electronics@gmail.com>
2026-03-02 19:07:43 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,28 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm lua
+pacman -Syu --noconfirm lua51
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package cuberite-git
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 wget https://download.cuberite.org/linux-$ARCH/Cuberite.tar.gz
 mkdir -p ./cuberite
 bsdtar -xvf Cuberite.tar.gz -C cuberite
-cd cuberite
-rm favicon.png
-rm LICENSE
-rm -rf ThirdPartyLicenses
-cd ..
+rm -rf cuberite/favicon.png cuberite/LICENSE cuberite/ThirdPartyLicenses
+VERSION="r$(grep -oP '#\K\d+' cuberite/buildinfo.txt)"
+echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 mv -v cuberite/* ./AppDir/bin
