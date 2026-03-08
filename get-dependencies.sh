c114de9ef70b42e70dfe2f6288165400f2d8f00c
Link4Electronics <link4electronics@gmail.com>
2026-03-08 07:58:55 -0300

    change lua to lua51

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm lua
+pacman -Syu --noconfirm lua51
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
