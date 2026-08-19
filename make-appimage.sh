e339e6d1109281b5b1bd233ac3c3b2b6ea9b785e
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:06:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,8 +12,6 @@ export ICON=https://raw.githubusercontent.com/cuberite/cuberite/refs/heads/maste
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
