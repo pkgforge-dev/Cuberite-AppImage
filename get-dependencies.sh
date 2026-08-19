20dca174b9114e2fb609c81d2a2d2c6bc6fc689c
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:06:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,12 +12,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package cuberite-git
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 wget https://download.cuberite.org/linux-$ARCH/Cuberite.tar.gz
